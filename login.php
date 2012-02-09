a66782d88bb395a54b5e4cd3d0ed476b4fbf28e2
Leon Bergmann <development@sky-lab.de>
2012-02-09 21:53:25 +0100

    Update login.php 9.12.2012 21:53
    + Kommentare für die Dokumentation

--- login.php
+++ login.php
@@ -1,14 +1,15 @@
 <?php
-// Autor : Leon Bergmann
-// Datum : 9.2.2012 
-// Aufgabe der Datei: †berprŸft ob der User berechtigt ist die Mediathek zu benutzen!
-
-// DafŸr wird mithilfe der Datenbank ŸberprŸft ob der User registriert ist. Danach wird das vom User eingegeben Passwort in einen SHA512 (http://de.wikipedia.org/wiki/Secure_Hash_Algorithm)
-// umgewandelt, danach wird das, fals der User vorhanden ist, Passwort aus der Datenbank ebenfalls in einen 
-// SHA512 Hash umgewandelt. Beide Hashes werden mit einander verglichen, sollte der eingegebene Hash dem aus der 
-// Datenbank nicht gleichen, wird der User auf eine Fehlerseite glenkt und muss gegebenenfalls das Passwort wiederholen!
-// Sollte das Passwort identisch sein wird eine Variable in der Session gesetzt die bei jedem Aufruf eine geschŸtzen Seite †berprŸft wird
-
+/** 
+* Autor : Leon Bergmann
+* Datum : 9.2.2012 21:52
+* Aufgabe der Datei: †berprŸft ob der User berechtigt ist die Mediathek zu benutzen!
+*
+* DafŸr wird mithilfe der Datenbank ŸberprŸft ob der User registriert ist. Danach wird das vom User eingegeben Passwort in einen SHA512 (http://de.wikipedia.org/wiki/Secure_Hash_Algorithm)
+* umgewandelt, danach wird das, fals der User vorhanden ist, Passwort aus der Datenbank ebenfalls in einen 
+* SHA512 Hash umgewandelt. Beide Hashes werden mit einander verglichen, sollte der eingegebene Hash dem aus der 
+* Datenbank nicht gleichen, wird der User auf eine Fehlerseite glenkt und muss gegebenenfalls das Passwort wiederholen!
+* Sollte das Passwort identisch sein wird eine Variable in der Session gesetzt die bei jedem Aufruf eine geschŸtzen Seite †berprŸft wird
+*/ 
 
 include "funktionen/connect.php"; // einbinden der Datenbankverbindung
 
@@ -29,24 +30,28 @@ else // Wenn der User in der Datenbank ist wird folgender Quellcode ausgefŸhrt
 {
  $resultOfQueryFetched = mysql_fetch_object($resultOfQuery); // Das Ergebnis der SQL Abfrage wird in ein Object geschrieben | mysql_fetch_object(http://de.php.net/manual/de/function.mysql-fetch-object.php) | Object (http://php.net/manual/en/language.types.object.php)
 
- $DatabaseUserPasswort = $resultOfQueryFetched->Passwort; // 
+ $DatabaseUserPasswort = $resultOfQueryFetched->Passwort; // Speichert das Passwort aus der Datenbank in einer Variable
  
- $DatabaseUserPasswortHashed	= hash('sha512',$DatabaseUserPasswort);
- $UserPasswortHashed			= hash('sha512',$UserPasswort);
  
- if($DatabaseUserPasswortHashed != $UserPasswortHashed)
+ $DatabaseUserPasswortHashed	= hash('sha512',$DatabaseUserPasswort); // bildet einen Hash aus dem Passwort aus der Datenbank
+ $UserPasswortHashed			= hash('sha512',$UserPasswort); // bildet einen Hash aus der Passworteingabe des Users
+ 
+ if($DatabaseUserPasswortHashed != $UserPasswortHashed) // PrŸfung ob sich beide Hashes gleichen
  {
-	echo "Passwörter sind nicht übereinstimmend!";
-	include "error.php";
+	// Wenn nein wird der User auf die Fehlerseite umgleitet und aufgefordert sein Passwort neu einzugeben 
+	$Fehler = "passwortIncorrect"; // Variable zu Behandlung der Ausgabe auf der Fehlerseite
+	include "error.php"; // einbinden der Fehlerseite
  }
+ 
  else
  {
-	session_start();
-	$_SESSION["sitepass"]=$passwort;
-	include "browser_info.php";
-	$_SESSION['UserOs'] = $osName.$osVersion;
-	$_SESSION['UserBrowser'] = $Browser;
-	include("main.php");
+	//wenn beide Passwšrter Ÿbereinstimmen wird eine Session eršffnet(http://de2.php.net/manual/de/intro.session.php)
+	session_start(); // šffnen der Session
+	$_SESSION["sitepass"]=$UserPasswortHashed; // Schreiben des Passwortes in die Session
+	include "browser_info.php"; // einbinden der browser_info.php um Informationen Ÿber den Browser zusammeln
+	$_SESSION['UserOs'] = $osName.$osVersion; // Speichern des Betriebssystems in der Session um bei spŠteren Aktionen diese Daten zu verwenden
+	$_SESSION['UserBrowser'] = $Browser; // Speichern des Browsers in der Session um bei spŠteren Aktionen diese Daten zu verwenden
+	include("main.php"); // einbinden der main.php | auf dieser Seite befinden sich alle weiteren Funktionen der Mediathek
 
  }
 }
