63857963cca870346a07195604eadd6dea483c7b
3delStahl <f.giller@sky-lab.de>
2012-03-01 15:52:21 +0100

    pass added

--- funktionen/connect.php
+++ funktionen/connect.php
@@ -1,9 +1,9 @@
 <?php
 //Serverconnection Daten
 $host		= "192.168.178.5"; //Server der Datenbank
-$user		= "root"; //User (mit allen rechten)| wird noch geändert
-// fŸr lokal test zwecke $pass		= "8uhbgt5";
+$user		= "root"; //User (mit allen rechten)| wird noch geï¿½ndert
+$pass		= "8uhbgt5";
 $db			= "mediathek";
-mysql_connect($host,$user);
+mysql_connect($host,$user,$pass);
 mysql_select_db($db);
 ?>
