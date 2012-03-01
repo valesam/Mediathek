98e0b09617ca16ff34983a79eea8632fe2712b5d
Leon Bergmann <development@sky-lab.de>
2012-03-01 15:57:18 +0100

    connect

--- funktionen/connect.php
+++ funktionen/connect.php
@@ -1,5 +1,6 @@
 <?php
 //Serverconnection Daten
+<<<<<<< HEAD
 $host		= "192.168.178.5"; //Server der Datenbank
 $user		= "root"; //User (mit allen rechten)| wird noch ge�ndert
 $pass		= "8uhbgt5";
@@ -7,3 +8,12 @@ $db			= "mediathek";
 mysql_connect($host,$user,$pass);
 mysql_select_db($db);
 ?>
+=======
+$host           = "localhost"; //Server der Datenbank
+$user           = "root"; //User (mit allen rechten)| wird noch gendert
+$pass           = "8uhbgt5";
+$db                     = "mediathek";
+mysql_connect($host,$user,$pass) or die(mysql_error()."connect");
+mysql_select_db($db) or die(mysql_error()."Select");
+?>
+>>>>>>> connect
