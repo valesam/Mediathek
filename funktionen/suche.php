35ce216950569507cc205dca5090eb7097acf410
Florian Giller <floriangiller@florians-macpro.fritz.box>
2012-03-02 17:09:54 +0100

    Neuen player eingebunden

--- funktionen/suche.php
+++ funktionen/suche.php
@@ -10,10 +10,11 @@ $AJAX 		= $_POST["ajax"];
 */
 //Hier DB abfrage der nötigen infos durch die den Film Titel
 #########################################################################
-$film_pfad = "/mediathek/uploads/video.mp4";
+$film_pfad = "..%2FMediathek%2Fuploads%2Fvideo.mp4";
 $film_preview = "";
 $film_titel = "Filmname";
 $film_beschreibung = "<h1>Das ist ein Test</h1>";
+$film_player_size = "width='470' height='320'";
 #########################################################################
 
 //$film = $_POST["film"];
@@ -22,5 +23,9 @@ $film_beschreibung = "<h1>Das ist ein Test</h1>";
 
 $film_preview = "/mediathek/player/preview.jpg";
 
-echo $film_pfad."#".$film_preview."#".$film_beschreibung;
+echo "<object ".$film_player_size."> <param name='movie' value='../Mediathek/player/StrobeMediaPlayback.swf'></param>
+<param name='flashvars' value='src=".$film_pfad."&post=".$film_preview."'></param>
+<param name='allowFullScreen' value='true'></param><param name='allowscriptaccess' value='always'></param>
+<param name='wmode' value='direct'></param><embed src='../Mediathek/player/StrobeMediaPlayback.swf' type='application/x-shockwave-flash' allowscriptaccess='always' allowfullscreen='true' wmode='direct' ".$film_player_size." flashvars='src=".$film_pfad."&post=".$film_preview."'></embed></object>";
+
 ?>
