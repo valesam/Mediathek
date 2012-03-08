728ffbd905e42c67ce7eb73581f93e823eb9ed73
3delStahl <f.giller@sky-lab.de>
2012-03-08 16:55:23 +0100

    Update upload.php

--- upload.php
+++ upload.php
@@ -14,12 +14,14 @@ if($dateityp[1] == "mp4" OR $dateityp[1] == "flv" OR $dateityp[1] == "3gp")
 	// Abfrage der Daten aus dem Vorherigen Formular
 	$Filmtitel = $_POST['Name']; // Daten aus dem Vormular
 	$Genre = $_POST['Genre'];// Daten aus dem Vormular
-	$FSK = $_POST['FSK'];// Daten aus dem Vormular
-	$Bewertung = $_POST['Bewertung'];// Daten aus dem Vormular
-	$file_name = $_POST['file_name'];// Daten aus dem Vormular
+	$Beschreibung = $_POST['Beschreibung'];// Daten aus dem Vormular
+	$file_name = $_POST['datei'];// Daten aus dem Vormular
+	$location = "http://webserver/Mediathek/uploads/".$dateiname;
 	
-	$sql = "INSERT INTO med_filme (Filmtitel,FSK,Bewertung,Genre) VALUES ('$Filmtitel','$FSK,$Bewertung','$Genre')";
-	echo $sql;
+	$sql = "INSERT INTO med_filme (Filmtitel,Genre,Beschreibung,SpeicherOrt) VALUES ('$Filmtitel','$Genre','$Beschreibung','$location')";
+	mysql_query($sql);
+	
+	
 	// Verbimdungs aufbau mit Abruch bei Fehlern
 	//mysql_connect($host,$user) or die (mysql_error().". Host");
 	//mysql_select_db($db) or die(mysql_error().". db");
@@ -30,7 +32,7 @@ if($dateityp[1] == "mp4" OR $dateityp[1] == "flv" OR $dateityp[1] == "3gp")
 }
 else
 {
-	echo "Der Film den Sie hochladen m&ouml;chten ist in einem nicht zul&auml;ssigen Format. Bitte konvertieren Sie ihn in ein geiegnetes Format HIER LINK FÜR ZUKÜCK EINFÜGEN UND DATEN AUS DEM FORMULAR WEIDER EINFÜGREN";
+	echo "Der Film (".$dateiname.") den Sie hochladen m&ouml;chten ist in einem nicht zul&auml;ssigen Format. Bitte konvertieren Sie ihn in ein geiegnetes Format HIER LINK FÜR ZUKÜCK EINFÜGEN UND DATEN AUS DEM FORMULAR WEIDER EINFÜGREN";
 }
 
 ?>
