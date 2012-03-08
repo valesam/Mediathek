a16509eeef67a32725f97a6e65a8193c2c2abe89
3delStahl <f.giller@sky-lab.de>
2012-03-08 15:56:22 +0100

    InfoText der upload.php erneuert

--- hochladen.php
+++ hochladen.php
@@ -1,9 +1,8 @@
 
-
 <h1>Datei Upload:</h1>
 
 <form action="upload.php" method="post" enctype="multipart/form-data">
-  <p>W&auml;hlen Sie eine Textdatei (txt, html usw.) von Ihrem Rechner aus:<br>
+  <p>W&auml;hlen Sie eine Videodatei (mp4, flv, 3gp usw.) von Ihrem Rechner aus, um sie hochzuladen<br>
     <input name="datei" type="file" size="50" maxlength="100000" accept="text/*">
 </p>
 <input type="submit" value="Hochladen">
