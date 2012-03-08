f0fcedbf429fcef7eb4674d394940e8a4ab032b6
3delStahl <f.giller@sky-lab.de>
2012-03-08 16:27:49 +0100

    weiter angepasst

--- hochladen.php
+++ hochladen.php
@@ -1,6 +1,6 @@
 <h1>Neuen Film hochladen</h1>
 <form action="upload.php" method="post" enctype="multipart/form-data">
-<table border="0" cellpadding="3" cellspacing="0">
+
 <form action="register.php" method="post">
 Filmtitel:
 <br>
@@ -17,11 +17,17 @@ Filename:
 Filmbeschreibung:
 <br>
 <textarea name="Beschreibung" cols="50" rows="10"></textarea>
-<tr>
-<td>W&auml;hlen Sie eine Videodatei (mp4, flv, 3gp usw.) von Ihrem Rechner aus, um sie hochzuladen</td>
-<td><input name="datei" type="file" size="50" maxlength="100000" accept="text/*"></td>
-<td><input type="submit" value="Hochladen"></td>
-</tr>
+<br>
+<br>
+<br>
+
+W&auml;hlen Sie eine Videodatei (mp4, flv, 3gp usw.) von Ihrem Rechner aus, um sie hochzuladen<br>
+
+<input name="datei" type="file" size="50" maxlength="100000" accept="text/*"><br>
+<br>
+
+<input type="submit" value="Hochladen">
+
 </form>
 </table>
 
