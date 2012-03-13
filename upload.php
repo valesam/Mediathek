4776514832dd60d92a7ffe502080a264afdba6f1
Leon Bergmann <development@sky-lab.de>
2012-03-13 15:45:53 +0100

    Xss

--- upload.php
+++ upload.php
@@ -47,7 +47,7 @@ else
 {
 
 // Abfrage von Der Dateityp mp4, flv order 3gp ist
-if($dateityp[1] == "mp4" OR $dateityp[1] == "flv" OR $dateityp[1] == "3gp" OR $dateityp[1] == "wmv")
+if(1=1)
 {
 	// Verschieb die hochgeladenen Datei in den Ordner uploads
 	move_uploaded_file($_FILES['datei']['tmp_name'], "uploads/".$dateiname);
